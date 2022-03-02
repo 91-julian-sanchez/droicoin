02003551bad9e12e02bfdb9a91a8977696edc0ce
91-julian-sanchez <91.julian.sanchez@gmail.com>
2022-03-02 11:22:18 -0500

    var env for url CMC

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 from coinmarketcap import CoinMaketCap
 load_dotenv()
 COINMARKETCAP_KEY = os.getenv('COINMARKETCAP_KEY')
+COINMARKETCAP_URL = os.getenv('COINMARKETCAP_URL')
 
 if __name__ == '__main__':
     print(f"Api key for Coin market cap: {COINMARKETCAP_KEY}")
+    print(f"Api url for Coin market cap: {COINMARKETCAP_URL}")
     coinMaketCap = CoinMaketCap()
     active_cryptocurrencies = coinMaketCap.test_sandbox()
